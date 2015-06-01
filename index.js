9332fb7847213122702d56fbde189d8ea9555911
Daniel X Moore <yahivin@gmail.com>
2015-06-01 12:43:37 -0700

    Updated in browser at hyperweb.space

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Yo!');
+  response.send('Yo dudester!');
 });
 
 app.listen(app.get('port'), function() {
